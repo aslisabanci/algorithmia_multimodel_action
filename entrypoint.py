0aa6424ede7d509dff5681499d048b6886c9c819
aslisabanci <aslisabanci@gmail.com>
2020-08-10 23:17:24 -0700

    Change params and algo call

--- entrypoint.py
+++ entrypoint.py
@@ -6,33 +6,29 @@ from action_src import call_algo
 
 if __name__ == "__main__":
     api_key = os.getenv("INPUT_API_KEY")
+    orchestrator_algo_path = os.getenv("ORCHESTRATOR_ALGO_PATH")
     parallel_algo_names = os.getenv("INPUT_PARALLEL_ALGO_NAMES")
     parallel_algo_inputs = os.getenv("INPUT_PARALLEL_ALGO_INPUT")
 
     repo_name = os.getenv("INPUT_PATH")
-    algo_hash = os.getenv("GITHUB_SHA")
-
     repo_path = "/github/workspace/{}".format(repo_name)
 
     if not api_key:
         raise Exception("field 'api_key' not defined in workflow")
+    if not orchestrator_algo_path:
+        raise Exception("field 'orchestrator_algo_path' not defined in workflow")
     if not parallel_algo_names:
         raise Exception("field 'parallel_algo_names' not defined in workflow")
     if not parallel_algo_inputs:
         raise Exception("field 'parallel_algo_inputs' not defined in workflow")
 
     if os.path.exists(repo_path):
-        with open("{}/{}".format(repo_path, "algorithmia.conf")) as f:
-            config_data = json.load(f)
-        algo_name = "{}/{}".format(config_data["username"], config_data["algoname"])
-
         algo_input_dict = {
             "parallel_algo_names": parallel_algo_names,
             "parallel_algo_inputs": parallel_algo_inputs,
         }
-
         algo_input = json.dumps(algo_input_dict)
-        call_algo(api_key, algo_name, algo_hash, algo_input)
+        call_algo(api_key, orchestrator_algo_path, algo_input)
     else:
         raise Exception(
             "actions/checkout on the local repo must be run before this action can be completed"
